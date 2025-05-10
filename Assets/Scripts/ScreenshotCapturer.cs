b280866389c55364f7295e81ade87e256913811a
Sanki <michal.sanki@gmail.com>
2025-05-10 17:00:42 +0200

    ScreenshotCapturer in progress

---
+++ Assets/Scripts/ScreenshotCapturer.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ScreenShotCapturer : MonoBehaviour
+{
+    [SerializeField] private List<Texture2D> images;
+
+    private void Update()
+    {
+        
+    }
+}
